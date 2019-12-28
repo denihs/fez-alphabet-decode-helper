c2c25fe743d919f4af85eb637d44a857355d3088
denyhs <denilsonh2014@gmail.com>
2019-12-28 15:53:48 -0400

    Adding features

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,91 @@
-import React from "react";
-import zero from "../public/images/0.jpg";
+import React, { Fragment, useState } from "react";
+import { AlphabetType } from "./homeHelper";
+import BackSpace from "./backspace-24px.svg";
+import Space from "./keyboard-space.png";
+import DeleteAll from "./delete_sweep-24px.svg";
+
+const Infos = ({ handleClick, values, grid, lastTyped }) => (
+        values.map(({ img, alt, representation }, idx) => (
+            <Fragment key={alt}>
+                <img
+                    onClick={() => handleClick(representation)}
+                    src={img}
+                    alt={alt}
+                    className={
+                        `icon ${lastTyped === representation ? "last-typed" : ""}`
+                    }
+                />
+                {(idx + 1) % grid === 0 ? <div /> : null}
+            </Fragment>
+        ))
+    )
+;
 
 export const Home = () => {
+    const numbersType = AlphabetType.getNumbersType();
+    const lettersType = AlphabetType.getLettersType();
+    const [word, setWord] = useState("");
+    const [lastTyped, setLastTyped] = useState("");
+
+    const handleClick = (alpha) => {
+        setWord(`${word} ${alpha}`);
+        setLastTyped(alpha);
+    };
+
+    const spaceCharacter = "_";
+
     return (
-        <img src={zero} />
+        <div className="home">
+            <div className="container">
+                <div>
+                    <p>Numbers</p>
+                    <Infos
+                        handleClick={handleClick}
+                        lastTyped={lastTyped}
+                        values={numbersType}
+                        grid={4}
+                    />
+                </div>
+                <div>
+                    <p>Letters</p>
+                    <Infos handleClick={handleClick}
+                           lastTyped={lastTyped}
+                           values={lettersType}
+                           grid={5}
+                    />
+                </div>
+            </div>
+
+            <img
+                className={lastTyped === spaceCharacter ? "last-typed" : ""}
+                onClick={() => handleClick(spaceCharacter)}
+                src={Space}
+                alt="space-keyboard"
+            />
+
+            <div className="answer-content">
+                <div className="answer-content-word">
+                    {word}
+                </div>
+                <div className="delete-buttons">
+                    <img
+                        className="icon"
+                        src={BackSpace}
+                        alt="back-space"
+                        onClick={() => {
+                            setWord(word.slice(0, word.length - 2));
+                        }}
+                    />
+                    <img
+                        className="icon"
+                        src={DeleteAll}
+                        alt="DeleteAll"
+                        onClick={() => {
+                            setWord("");
+                        }}
+                    />
+                </div>
+            </div>
+        </div>
     )
-}
+};
---
+++ src/homeHelper.js
@@ -0,0 +1,166 @@
+import zero from "./images/0.jpg";
+import one from "./images/1.jpg";
+import two from "./images/2.jpg";
+import threeA from "./images/3a.jpg";
+import threeB from "./images/3b.jpg";
+import fourA from "./images/4a.jpg";
+import fourB from "./images/4b.jpg";
+import fiveA from "./images/5a.jpg";
+import fiveB from "./images/5b.jpg";
+import sixA from "./images/6a.jpg";
+import sixB from "./images/6b.jpg";
+import sevenA from "./images/7a.jpg";
+import sevenB from "./images/7b.jpg";
+import eight from "./images/8.jpg";
+import nine from "./images/9.jpg";
+import ten from "./images/10.jpg";
+import A from "./images/A.jpg";
+import B from "./images/B.jpg";
+import C from "./images/C.jpg";
+import D from "./images/D.jpg";
+import E from "./images/E.jpg";
+import F from "./images/F.jpg";
+import G from "./images/G.jpg";
+import H from "./images/H.jpg";
+import I from "./images/I.jpg";
+import J from "./images/J.jpg";
+import K from "./images/K.jpg";
+import L from "./images/L.jpg";
+import M from "./images/M.jpg";
+import N from "./images/N.jpg";
+import O from "./images/O.jpg";
+import P from "./images/P.jpg";
+import Q from "./images/Q.jpg";
+import R from "./images/R.jpg";
+import S from "./images/S.jpg";
+import T from "./images/T.jpg";
+import U from "./images/U.jpg";
+import W from "./images/W.jpg";
+import X from "./images/X.jpg";
+import Y from "./images/Y.jpg";
+import Z from "./images/Z.jpg";
+
+export const NUMBER_TYPE = "NUMBER";
+export const LETTER_TYPE = "LETTER";
+
+const images = {
+    A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, W, X, Y, Z
+};
+
+const alphabet = {
+    ZERO: {
+        alt: "zero",
+        img: zero,
+        representation: 0,
+        type: NUMBER_TYPE,
+    },
+    ONE: {
+        alt: "one",
+        img: one,
+        representation: 1,
+        type: NUMBER_TYPE,
+    },
+    TWO: {
+        alt: "two",
+        img: two,
+        representation: 2,
+        type: NUMBER_TYPE,
+    },
+    THREE_A: {
+        alt: "threeA",
+        img: threeA,
+        representation: 3,
+        type: NUMBER_TYPE,
+    },
+    THREE_B: {
+        alt: "threeB",
+        img: threeB,
+        representation: 3,
+        type: NUMBER_TYPE,
+    },
+    FOUR_A: {
+        alt: "fourA",
+        img: fourA,
+        representation: 4,
+        type: NUMBER_TYPE,
+    },
+    FOUR_B: {
+        alt: "fourB",
+        img: fourB,
+        representation: 4,
+        type: NUMBER_TYPE,
+    },
+    FIVE_A: {
+        alt: "fiveA",
+        img: fiveA,
+        representation: 5,
+        type: NUMBER_TYPE,
+    },
+    FIVE_B: {
+        alt: "fiveB",
+        img: fiveB,
+        representation: 5,
+        type: NUMBER_TYPE,
+    },
+    SIX_A: {
+        alt: "sixA",
+        img: sixA,
+        representation: 6,
+        type: NUMBER_TYPE,
+    },
+    SIX_B: {
+        alt: "sixB",
+        img: sixB,
+        representation: 6,
+        type: NUMBER_TYPE,
+    },
+    SEVEN_A: {
+        alt: "sevenA",
+        img: sevenA,
+        representation: 7,
+        type: NUMBER_TYPE,
+    },
+    SEVEN_B: {
+        alt: "sevenB",
+        img: sevenB,
+        representation: 7,
+        type: NUMBER_TYPE,
+    },
+    EIGHT: {
+        alt: "eight",
+        img: eight,
+        representation: 8,
+        type: NUMBER_TYPE,
+    },
+    NINE: {
+        alt: "nine",
+        img: nine,
+        representation: 9,
+        type: NUMBER_TYPE,
+    },
+    TEN: {
+        alt: "ten",
+        img: ten,
+        representation: 10,
+        type: NUMBER_TYPE,
+    },
+    ...(() => {
+        const alpha = Object.keys(images);
+        return alpha.reduce((acc, letter) => (
+            {
+                ...acc,
+                [letter]: {
+                    alt: letter,
+                    img: images[letter],
+                    representation: letter,
+                    type: LETTER_TYPE,
+                }
+            }
+        ), {});
+    })()
+};
+
+alphabet.getNumbersType = () => Object.values(alphabet).filter(a => a.type === NUMBER_TYPE);
+alphabet.getLettersType = () => Object.values(alphabet).filter(a => a.type === LETTER_TYPE);
+
+export const AlphabetType = alphabet;
