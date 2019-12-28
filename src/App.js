812bd68cc8b538bed215fd83a50914d889be2402
denyhs <denilsonh2014@gmail.com>
2019-12-28 16:40:50 -0400

    Adjusts

--- src/App.js
+++ src/App.js
@@ -6,6 +6,15 @@ function App() {
   return (
     <div className="App">
       <Home />
+      <footer>
+          <a
+              href="https://github.com/denihs/fez-alphabet-decode-helper"
+              target="_blank"
+              rel="noopener noreferrer"
+          >
+              DenyHs
+          </a>
+      </footer>
     </div>
   );
 }
